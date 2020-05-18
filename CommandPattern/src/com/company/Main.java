cdc765d574de05379807d1815b84e0d2fcd27b7e
budopest <abdo.mohamed5111995@gmail.com>
2020-05-18 06:20:08 +0200

    command pattern

---
+++ CommandPattern/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Lamp firstLamp = new Lamp();
+        Lamp secondLamp = new Lamp();
+        Invoker remote = new Invoker();
+        // set first command to turn on first lamp
+        remote.setCommand(new turnONCommand(firstLamp));
+        // set second command to turn on second lamp
+        remote.setCommand(new turnONCommand(secondLamp));
+        // execute the commands
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of the command: ");
+        int commandNumber = scanner.nextInt();
+
+        switch (commandNumber){
+            case 0 :
+                remote.execute(commandNumber);
+                break;
+            case 1 :
+                remote.execute(commandNumber);
+                break;
+
+        }
+
+
+
+    }
+}
